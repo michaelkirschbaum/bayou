e03ec1438a25fa7325eea3620098fd1fb3afff5f
Michael Kirschbaum <mkirsch@clemson.edu>
2016-11-19 16:23:23 -0800

    correct config

--- bayou.py
+++ bayou.py
@@ -8,6 +8,6 @@ class Bayou(object):
         pass
     index.exposed = True
 
-cherrypy.config.update({'server.socket_host': '0.0.0.0',})
-cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
+cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000'))})
 cherrypy.quickstart(Bayou())
