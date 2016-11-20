11692331b3b75e4563d92d805008ccc620eb3a15
Michael Kirschbaum <mkirsch@clemson.edu>
2016-11-19 16:19:10 -0800

    add config

--- bayou.py
+++ bayou.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import cherrypy
+import os
 
 class Bayou(object):
     def index(self):
@@ -8,4 +9,5 @@ class Bayou(object):
     index.exposed = True
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
 cherrypy.quickstart(Bayou())
